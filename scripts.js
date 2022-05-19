159985d2f54ddc1f744d15d235157c8cf252aeb9
Ben Kiboma <omayioben@gmail.com>
2022-05-19 13:59:04 +0300

    style mobile page

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const hamBug = document.querySelector('.hamburger');
+const crossBut = document.querySelector('.cross-button');
 const navMenu = document.querySelector('.lower-mennu-content');
 const body = document.querySelector('body');
 
@@ -6,6 +7,12 @@ hamBug.addEventListener('click', () => {
   hamBug.classList.toggle('active');
   navMenu.classList.toggle('active');
   body.classList.toggle('no-scroll');
+});
+
+crossBut.addEventListener('click', () => {
+  hamBug.classList.remove('active');
+  navMenu.classList.remove('active');
+  body.classList.remove('no-scroll');
 });
 
 document.querySelectorAll('.nav_link').forEach((n) =>
